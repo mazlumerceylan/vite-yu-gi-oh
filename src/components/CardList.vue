07b0ce3785dac3d684321112fa32ee9a6c5cd219
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-05-03 05:18:01 +0200

    loader added

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -2,13 +2,19 @@
   <div class="card-list">
     <p class="total-cards">Total Cards: {{ totalResults }}</p>
     <div class="card-grid">
-      <div class="card" v-for="card in cards" :key="card.id">
+      <div v-if="isLoading" class="loading-spinner">
+        <div class="lds-spinner">
+          <div v-for="n in 12" :key="n"></div>
+        </div>
+      </div>
+      <div class="card" v-for="card in cards" :key="card.id" v-else>
         <img :src="card.card_images[0].image_url" alt="Card image" />
         <p>{{ card.name }}</p>
       </div>
     </div>
   </div>
 </template>
+
 <script>
 import axios from 'axios';
 
@@ -16,16 +22,19 @@ export default {
   data() {
     return {
       cards: [],
-      totalResults: 0
+      totalResults: 0,
+      isLoading: true
     };
   },
   async created() {
     try {
       const response = await axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php');
       this.cards = response.data.data;
-      this.totalResults = this.cards.length; // Calcola il totale delle carte utilizzando la lunghezza dell'array
+      this.totalResults = this.cards.length;
+      this.isLoading = false;
     } catch (error) {
       console.error('Error fetching data:', error);
+      this.isLoading = false;
     }
   }
 };
@@ -33,6 +42,5 @@ export default {
 
 
 <style lang="sass">
-
 
 </style>
