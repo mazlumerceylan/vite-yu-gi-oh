18c066b3aa835dc364f2888a93e1edc9ec08a20d
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-05-03 06:28:30 +0200

    import´s update

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
   <div id="app">
     <Header />
+    <OptionsSection />
     <CardList />
   </div>
 </template>
 
 <script>
 import CardList from './components/CardList.vue';
+import OptionsSection from './components/OptionsSection.vue';
 import Header from './components/Header.vue';
 
 export default {
   components: {
     Header,
+    OptionsSection,
     CardList
   }
 };
 </script>
 
 <style lang="sass">
- @import './src/scss/CardList.scss'
- @import './src/scss/General.scss'
- @import './src/scss/Header.scss'
+@import './src/scss/General.scss'
+@import './src/scss/Header.scss'
+@import './src/scss/OptionsSection.scss'
+@import './src/scss/CardList.scss'
 
 </style>
