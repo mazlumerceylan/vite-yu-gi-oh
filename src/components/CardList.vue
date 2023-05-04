89f18cb49028d1da39a759116e10b0cfe6e04e2d
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-05-04 05:21:15 +0200

    SearchBar added

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="card-list">
-    <p class="total-cards">Total Cards: {{ totalResults }}</p>
-    <div class="card-grid">
-      <div v-if="isLoading" class="loading-spinner">
-        <div class="lds-spinner">
-          <div v-for="n in 12" :key="n"></div>
+  <div>
+    <search-bar @search="search" @reset="reset"></search-bar>
+    <div class="card-list">
+      <p class="total-cards">Totale Carte: {{ totalResults }}</p>
+      <div class="card-grid">
+        <div v-if="isLoading" class="loading-spinner">
+          <div class="lds-spinner">
+            <div v-for="n in 12" :key="n"></div>
+          </div>
         </div>
-      </div>
-      <div class="card" v-for="card in cards" :key="card.id" v-else>
-        <img :src="card.card_images[0].image_url" alt="Card image" />
-        <p>{{ card.name }}</p>
+        <div class="card" v-for="card in filteredCards" :key="card.id" v-else>
+          <img :src="card.card_images[0].image_url" alt="Immagine Carta" />
+          <p>{{ card.name }}</p>
+          <p v-if="card.archetype">Archetipo: {{ card.archetype }}</p>
+          <p v-else>Archetipo: N/D</p>
+        </div>
       </div>
     </div>
   </div>
@@ -17,13 +22,18 @@
 
 <script>
 import axios from 'axios';
+import SearchBar from './SearchBar.vue';
 
 export default {
+  components: {
+    SearchBar
+  },
   data() {
     return {
       cards: [],
+      filteredCards: [],
       totalResults: 0,
-      isLoading: true
+      isLoading: true,
     };
   },
   async created() {
@@ -31,16 +41,35 @@ export default {
       const response = await axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php');
       this.cards = response.data.data;
       this.totalResults = this.cards.length;
+      this.filteredCards = this.cards;
       this.isLoading = false;
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Errore nel recupero dei dati:', error);
       this.isLoading = false;
+    }
+  },
+  methods: {
+    search(searchText) {
+      if (searchText) {
+        this.filteredCards = this.cards.filter(card => {
+          const searchLowerCase = searchText.toLowerCase();
+          const nameLowerCase = card.name.toLowerCase();
+          const archetypeLowerCase = card.archetype ? card.archetype.toLowerCase() : '';
+          return nameLowerCase.includes(searchLowerCase) || archetypeLowerCase.includes(searchLowerCase);
+        });
+        this.totalResults = this.filteredCards.length;
+      } else {
+        this.filteredCards = this.cards;
+        this.totalResults = this.cards.length;
+      }
+    },
+    reset() {
+      this.filteredCards = this.cards;
+      this.totalResults = this.cards.length;
     }
   }
 };
 </script>
 
-
 <style lang="sass">
-
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input type="text" v-model="searchText" placeholder="Nome o Archetipo" />
+    <button @click="search">Cerca</button>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchText: ''
+    };
+  },
+  methods: {
+    search() {
+      this.$emit('search', this.searchText.trim());
+    },
+    reset() {
+      this.$emit('reset');
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
