4d269a15b7950f6cc034184ab78a327c4a734f3a
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-05-04 06:01:41 +0200

    last fix

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,12 @@
 
 <script>
 import CardList from './components/CardList.vue';
-import ArchetypeFilter from './components/ArchetypeFilter.vue';
 import Header from './components/Header.vue';
+
 
 export default {
   components: {
     Header,
-    ArchetypeFilter,
     CardList
   }
 };
@@ -23,7 +22,6 @@ export default {
 <style lang="sass">
 @import './src/scss/General.scss'
 @import './src/scss/Header.scss'
-@import './src/scss/OptionsSection.scss'
 @import './src/scss/CardList.scss'
 @import './src/scss/SearchBar.scss'
 
